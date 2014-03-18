58071b02b6aec524be3087f257abc51b599688a7
bsautermeister <apps@bsautermeister.de>
2014-03-18 22:26:12 +0100

    Added resources (tiles, images, sounds)

---
+++ Whip.App/Controls/LocalizedAboutControl.cs
@@ -0,0 +1,38 @@
+﻿using PhoneKit.Framework.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whip.App.Resources;
+
+namespace Whip.App.Controls
+{
+    /// <summary>
+    /// The localized About control.
+    /// </summary>
+    public class LocalizedAboutControl : AboutControlBase
+    {
+        /// <summary>
+        /// Localizes the user controls contents and texts.
+        /// </summary>
+        protected override void LocalizeContent()
+        {
+            ApplicationIconSource = new Uri("/Assets/ApplicationIcon.png", UriKind.Relative);
+            ApplicationTitle = AppResources.ApplicationTitle;
+            ApplicationVersion = AppResources.ApplicationVersion;
+            ApplicationAuthor = AppResources.ApplicationAuthor;
+            ApplicationDescription = AppResources.ApplicationDescription;
+            SupportAndFeedbackText = AppResources.SupportAndFeedback;
+            SupportAndFeedbackEmail = "apps@bsautermeister.de";
+            PrivacyInfoText = AppResources.PrivacyInfo;
+            PrivacyInfoLink = "http://bsautermeister.de/privacy.php";
+            RateAndReviewText = AppResources.RateAndReview;
+            MoreAppsText = AppResources.MoreApps;
+            MoreAppsSearchTerms = "Benjamin Sautermeister";
+
+            // contributors
+            ContributorsListVisibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
